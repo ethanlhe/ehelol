6ab59a58fc3357361ff6fd0d82c74ef355a2c7a4
Ethan He <ethansp123@gmail.com>
2025-02-26 09:57:55 +0000

    Improve mobile responsiveness
    Update layout to be more mobile-friendly.  When the screen is narrow, the layout will switch to a simpler, vertically stacked arrangement of equal-sized boxes.

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -37,40 +37,68 @@ const Index = () => {
   return (
     <TooltipProvider delayDuration={0}>
       <main className={`min-h-screen p-8 md:p-16 flex items-center transition-colors duration-300 ${theme === "dark" ? "bg-[#111111] text-white" : "bg-white text-black"}`}>
-        <div className="relative w-full flex justify-center" style={{ marginLeft: "280px" }}>
-          <div className="relative w-full max-w-4xl space-y-2">
-            {/* Top Row */}
-            <div className="flex gap-2">
-              <div className="h-[275px] w-[275px]">
+        <div className="relative w-full flex justify-center">
+          <div className="w-full md:w-auto space-y-2">
+            {/* Mobile Layout - All sections stacked */}
+            <div className="block md:hidden space-y-2 max-w-sm mx-auto">
+              <div className="h-[275px]">
                 <NameSection theme={theme} boxClasses={`${boxClasses} h-full`} />
               </div>
-              <div className="h-[275px] w-[275px]">
+              <div className="h-[275px]">
                 <AboutSection theme={theme} boxClasses={`${boxClasses} h-full`} />
               </div>
-            </div>
-
-            {/* Middle Row - Projects, Blog and Contact */}
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-2">
-              <div className="relative md:col-span-1">
-                <div className="h-[135px]">
-                  <BlogSection theme={theme} boxClasses={`${boxClasses} h-full`} />
-                </div>
-                <div className="absolute left-0 top-0 -translate-x-[calc(100%+8px)] w-full h-[135px] hidden md:block">
-                  <ProjectsSection theme={theme} boxClasses={`${boxClasses} h-full`} />
-                </div>
+              <div className="h-[135px]">
+                <ProjectsSection theme={theme} boxClasses={`${boxClasses} h-full`} />
               </div>
-              <div className="col-span-1 md:col-span-2 h-[135px]">
+              <div className="h-[135px]">
+                <BlogSection theme={theme} boxClasses={`${boxClasses} h-full`} />
+              </div>
+              <div className="h-[135px]">
                 <ContactSection theme={theme} boxClasses={`${boxClasses} h-full`} />
               </div>
-            </div>
-
-            {/* Bottom Row - Experience and Theme */}
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-2">
-              <div className="col-span-2 h-[135px]">
+              <div className="h-[135px]">
                 <ExperienceSection theme={theme} boxClasses={`${boxClasses} h-full`} />
               </div>
               <div className="h-[135px]">
                 <ThemeSection theme={theme} boxClasses={`${boxClasses} h-full`} setTheme={setTheme} />
+              </div>
+            </div>
+
+            {/* Desktop Layout - Original asymmetric design */}
+            <div className="hidden md:block relative w-full max-w-4xl" style={{ marginLeft: "280px" }}>
+              {/* Top Row */}
+              <div className="flex gap-2">
+                <div className="h-[275px] w-[275px]">
+                  <NameSection theme={theme} boxClasses={`${boxClasses} h-full`} />
+                </div>
+                <div className="h-[275px] w-[275px]">
+                  <AboutSection theme={theme} boxClasses={`${boxClasses} h-full`} />
+                </div>
+              </div>
+
+              {/* Middle Row - Projects, Blog and Contact */}
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-2 mt-2">
+                <div className="relative md:col-span-1">
+                  <div className="h-[135px]">
+                    <BlogSection theme={theme} boxClasses={`${boxClasses} h-full`} />
+                  </div>
+                  <div className="absolute left-0 top-0 -translate-x-[calc(100%+8px)] w-full h-[135px]">
+                    <ProjectsSection theme={theme} boxClasses={`${boxClasses} h-full`} />
+                  </div>
+                </div>
+                <div className="col-span-1 md:col-span-2 h-[135px]">
+                  <ContactSection theme={theme} boxClasses={`${boxClasses} h-full`} />
+                </div>
+              </div>
+
+              {/* Bottom Row - Experience and Theme */}
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-2 mt-2">
+                <div className="col-span-2 h-[135px]">
+                  <ExperienceSection theme={theme} boxClasses={`${boxClasses} h-full`} />
+                </div>
+                <div className="h-[135px]">
+                  <ThemeSection theme={theme} boxClasses={`${boxClasses} h-full`} setTheme={setTheme} />
+                </div>
               </div>
             </div>
           </div>
