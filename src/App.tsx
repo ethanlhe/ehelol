ad6995e1bfb4e01e8c4ff77d84728196d1cc5746
Ethan He <42473043+ethanlhe@users.noreply.github.com>
2025-07-07 19:30:17 -0700

    update page

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter basename={import.meta.env.BASE_URL}>
+        <BrowserRouter basename="/ehelol">
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/portfolio" element={<Portfolio />} />
